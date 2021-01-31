1c86d977e37946932a1490ad5bbaa9f1a7e192bd
termisam005 <termi0004@gmail.com>
2021-02-01 01:54:02 +0900

    first templete

---
+++ js/com_ui.js
@@ -0,0 +1,19 @@
+
+/*$(function(){
+	$(".text_space").click(function(){
+		$(this).closest(".ch_text").toggleClass("on").siblings().removeClass("on");
+	});
+	$(".btn_del").click(function(){
+		$(this).closest(".ch_text").removeClass("on");
+	});
+});
+
+$(function(){
+	$(".btn_set").click(function(){
+		$(this).closest(".btbox").toggleClass("on").siblings().removeClass("on");
+	});
+	$(".btn_del").click(function(){
+		$(this).closest(".btbox").removeClass("on");
+		$(this).closest(".btbox").addClass("del");
+	});
+});*/
